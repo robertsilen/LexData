79c461561c1ae21b93a540a84f0d97a2033da851
Michael F. Schönitzer <michael@schoenitzer.de>
2019-09-29 00:30:47 +0200

    Fix example from docs

--- example.py
+++ example.py
@@ -12,18 +12,18 @@ print(L2.claims.keys())
 # and Forms
 print(len(L2.forms))
 F1 = L2.forms[0]
-print(F1.claims.keys())
+print(F1.claims().keys)
 # and senses
 print(len(L2.senses))
 S1 = L2.senses[0]
-print(S1.claims.keys())
+print(S1.claims().keys)
 
 # Find or create a Lexeme by lemma, language and grammatical form
 L2 = LexData.get_or_create_lexeme(repo, "first", en, "Q1084")
 
 # You can easily create forms…
 if len(L2.forms) == 0:
-    L2.createForm("firsts", "Q146786")
+    L2.createForm("firsts", ["Q146786"])
 
 # …or senses, with or without additional claims
 if len(L2.senses) == 0:
